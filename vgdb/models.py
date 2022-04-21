15e337071a6615926a32c63ab5bd97dedb29e63a
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-21 20:44:00 +0000

    saving created Company and Franchise models but relationshiips still need to be added

--- vgdb/models.py
+++ vgdb/models.py
@@ -9,3 +9,10 @@ class Game(models.Model):
     release_date = models.DateField(null=True)
     coverart = models.URLField(max_length=500)
     expansions = models.CharField(max_length=200)
+
+class Franchise(models.Model):
+    title = models.CharField(max_length=200)
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    about = models.CharField(max_length=2000)
