b743e31c913e64c095eb61a49f57a779d3ff37ce
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-25 15:13:12 +0000

    added rest_framework to Settings file for installed apps

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,6 +31,7 @@ CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'vgdb.apps.VgdbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
